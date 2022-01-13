0a080aff9906e6bcedb73fef2b5eb65f8818332e
GVJM-UFRJ <83138049+GVJM-UFRJ@users.noreply.github.com>
2022-01-13 19:14:04 -0300

    Meio e fim de jogo adicionados

--- src/br/com/letscode/batalhanaval/Board.java
+++ src/br/com/letscode/batalhanaval/Board.java
@@ -1,5 +1,7 @@
 package br.com.letscode.batalhanaval;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -21,128 +23,9 @@ public class Board {
     //Lista de caracteres utilizados nas linhas da tabela
     final char[] linesLetters= {'A','B','C','D','E','F','G','H','I','J'};
 
-    //Função para incluir novos barcos
-    public boolean setBoat(int line, int column, boolean player){
-        //Variável que é retornada como true se a operação foi um sucesso ou false caso não(caso aquele dado já tenha sido preenchido)
-        boolean success = true;
-
-        if(player){
-            if(boardArray[line][column][0]==0){
-                boardArray[line][column][0]=1;
-            }
-            else{ success = false; }
-        }
-        else{
-            if(boardArray[line][column][1]==0){
-                boardArray[line][column][1]=1;
-            }
-            else{ success = false; }
-        }
-
-        return success;
-    }
-
-    //Função para incluir novos tiros
-    public boolean setShot(int line, int column, boolean player){
-        //Variável que avisa se a operação foi um sucesso ou não(caso aquele dado já tenha sido preenchido)
-        boolean success = true;
-
-        if(player){
-            if(boardArray[line][column][2]==0){
-                boardArray[line][column][2]=1;
-                if(boardArray[line][column][1]==1){
-                    playerScore++;
-                }
-            }
-            else{ success = false; }
-        }
-        else{
-            if(boardArray[line][column][3]==0){
-                boardArray[line][column][3]=1;
-                if(boardArray[line][column][0]==1){
-                    cpuScore++;
-                }
-            }
-            else{ success = false; }
-        }
-
-        return success;
-    }
-
-    //Função para gerar navios do computador
-    public void cpuShips(){
-        //Loop while para inserção dos navios nos indices [line][column] a partir de inteiros aleatórios,
-        //com verificação de valores válidos.
-        int count = 0;
-        while(count<10){
-            int line = ThreadLocalRandom.current().nextInt(0, 10);
-            int column = ThreadLocalRandom.current().nextInt(0, 10);
-            if(setBoat(line,column,false)){
-                count++;
-            }
-        }
-    }
-
-    //Função para incluir navios do jogador
-    public void playerShips(){
-        Scanner scanner = new Scanner(System.in);
-
-        printBoard();
-
-        //Loop para inserção dos barcos do jogador
-        int count = 0;
-        while(count<10){
-
-            int lineIndex;
-            int columnIndex;
-
-            System.out.printf("Informe a linha do seu barco nº%d (use somente letras maiúsculas): ",(count+1));
-            char letterInput = scanner.next().charAt(0);
-
-            Boolean invalidLine = true;
-
-            for (int i = 0; i < linesLetters.length; i++) {
-                if( linesLetters[i] == letterInput ){
-                    lineIndex=i;
-                    invalidLine=false;
-                    boolean invalidColumn = true;
-
-                    while(invalidColumn){
-                        System.out.printf("Informe a coluna do seu barco nº%d (use somente números de 0 à 9): ",(count+1));
-                        int columnInput = scanner.nextInt();
-
-                        if(0<=columnInput && columnInput<=9){
-
-                            columnIndex= columnInput;
-
-                            //Realiza adição do valor. Caso a função retorne falso,
-                            //este espaço já fora preenchido e não iremos contar esta ação.
-                            if(setBoat(lineIndex,columnIndex,true)){
-                                invalidColumn=false;
-                                count++;
-
-                                //Imprime próxima board
-                                printBoard();
-                            }
-                            else{
-                                System.out.println("Coluna inválida.");
-                            }
-                        }
-                        else{
-                            System.out.println("Valor inválido, tente novamente.");
-                        }
-                    }
-                }
-            }
-
-            if(invalidLine){
-                System.out.println("Valor inválido, tente novamente.");
-            }
-        }
-    }
-
+    //Função que imprime no console o tabuleiro do jogador
     public void printBoard(){
-        System.out.println("---------------------------------------------");
+        System.out.println("\n\n\n\n---------------------------------------------");
         System.out.println("                   JOGADOR");
         System.out.println("---------------------------------------------");
         System.out.println("|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
@@ -201,7 +84,140 @@ public class Board {
             //System.out.println("---------------------------------------------");
 
         }
-        System.out.println("Placar:    [Jogador-"+playerScore+" | Computador-"+cpuScore+"]");
+        System.out.println("---------------------------------------------");
+
+        System.out.println("\nPlacar:    [Jogador-"+playerScore+" | Computador-"+cpuScore+"]\n");
+    }
+
+    //Função para incluir novos barcos
+    public boolean setBoat(int line, int column, boolean player){
+        //Variável que é retornada como true se a operação foi um sucesso ou false caso não(caso aquele dado já tenha sido preenchido)
+        boolean success = true;
+
+        if(player){
+            if(boardArray[line][column][0]==0){
+                boardArray[line][column][0]=1;
+            }
+            else{ success = false; }
+        }
+        else{
+            if(boardArray[line][column][1]==0){
+                boardArray[line][column][1]=1;
+            }
+            else{ success = false; }
+        }
+
+        return success;
+    }
+
+    //Função para incluir novos tiros
+    public boolean setShot(int line, int column, boolean player){
+        //Variável que avisa se a operação foi um sucesso ou não(caso aquele dado já tenha sido preenchido)
+        boolean success = true;
+
+        if(player){
+            if(boardArray[line][column][2]==0){
+                boardArray[line][column][2]=1;
+                if(boardArray[line][column][1]==1){
+                    playerScore++;
+                }
+            }
+            else{ success = false; }
+        }
+        else{
+            if(boardArray[line][column][3]==0){
+                boardArray[line][column][3]=1;
+                if(boardArray[line][column][0]==1){
+                    cpuScore++;
+                }
+            }
+            else{ success = false; }
+        }
+
+        return success;
+    }
+
+    //Função para gerar 10 navios aleatórios
+    public void randomShips(Boolean player){
+        //Loop while para inserção dos navios nos indices [line][column] a partir de inteiros aleatórios,
+        //com verificação de valores válidos.
+        int count = 0;
+        while(count<10){
+            int line = ThreadLocalRandom.current().nextInt(0, 10);
+            int column = ThreadLocalRandom.current().nextInt(0, 10);
+            if(setBoat(line,column,player)){
+                System.out.printf("[ %d , %d ] \n",line,column);
+                count++;
+            }
+        }
+    }
+
+    //Função para inserção manual dos navios do jogador
+    public void playerShips(){
+        Scanner scanner = new Scanner(System.in);
+
+        printBoard();
+        System.out.print("Caso queira inserir seus navios manualmente digite SIM(maiúsculo), caso contrário digite qualquer outro valor:");
+        String insercaoManual = scanner.next().trim();
+        System.out.println(insercaoManual.equals("SIM"));
+        if(insercaoManual.equals("SIM")){
+            //Loop para inserção dos barcos do jogador
+            int count = 0;
+            while(count<10){
+
+                int lineIndex;
+                int columnIndex;
+
+                System.out.printf("Informe a linha do seu barco nº%d (use somente letras maiúsculas): ",(count+1));
+                char letterInput = scanner.next().charAt(0);
+
+                Boolean invalidLine = true;
+
+                for (int i = 0; i < linesLetters.length; i++) {
+                    if( linesLetters[i] == letterInput ){
+                        lineIndex=i;
+                        invalidLine=false;
+                        boolean invalidColumn = true;
+
+                        while(invalidColumn){
+                            System.out.printf("Informe a coluna do seu barco nº%d (use somente números de 0 à 9): ",(count+1));
+                            int columnInput = scanner.nextInt();
+
+                            if(0<=columnInput && columnInput<=9){
+
+                                columnIndex= columnInput;
+
+                                //Realiza adição do valor. Caso a função retorne falso,
+                                //este espaço já fora preenchido e não iremos contar esta ação.
+                                if(setBoat(lineIndex,columnIndex,true)){
+                                    invalidColumn=false;
+                                    count++;
+
+                                    //Imprime próxima board
+                                    printBoard();
+                                }
+                                else{
+                                    System.out.println("Coluna inválida.");
+                                }
+                            }
+                            else{
+                                System.out.println("Valor inválido, tente novamente.");
+                            }
+                        }
+                    }
+                }
+
+                if(invalidLine){
+                    System.out.println("Valor inválido, tente novamente.");
+                }
+            }
+
+        }
+
+        else {
+            randomShips(true);
+        }
+
     }
 
     //Função de tiros do cpu
@@ -211,6 +227,7 @@ public class Board {
             int line = ThreadLocalRandom.current().nextInt(0, 10);
             int column = ThreadLocalRandom.current().nextInt(0, 10);
             if(setShot(line,column,false)){
+                System.out.printf("[ %d , %d ] \n",line,column);
                 invalidShot=false;
             }
         }
@@ -250,9 +267,6 @@ public class Board {
                             if(setShot(lineIndex,columnIndex,true)){
                                 invalidColumn=false;
                                 validCoord=true;
-
-                                //Imprime próxima board
-                                printBoard();
                             }
                             else{
                                 System.out.println("Coluna inválida.");
@@ -272,7 +286,27 @@ public class Board {
 
     }
 
+    public void gameEnding(){
+
+        if(playerIsWinner){
+            System.out.println("Parabéns!!! Você venceu.");
+        }
+        else{
+            System.out.println("O computador venceu!");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Novo jogo (digite SIM ou NAO): ");
+
+        String novoJogo = scanner.next();
+        if(novoJogo.equals("SIM")){
+            gameInit();
+        }
+    }
+
     public void gameInit(){
+
 
         //Preencher tabela com zeros
         for (int i = 0; i < boardArray.length; ++i) {
@@ -289,7 +323,8 @@ public class Board {
 
         playerShips();
         
-        cpuShips();
+        randomShips(false);
+
         while(cpuScore<10||playerScore<10){
             playerShoots();
             if(playerScore>=10){
@@ -301,9 +336,10 @@ public class Board {
                 playerIsWinner=false;
                 break;
             }
-
+            printBoard();
         }
 
+        gameEnding();
     }
 
     public static void main(String[] args) {
